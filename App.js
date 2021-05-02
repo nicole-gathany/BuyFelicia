161434cfd7e167131b54e041b9221bde3eecf0ef
nicole-gathany <nicole.gathany@gmail.com>
2021-05-02 14:25:21 -0400

    just playing around with ReactNative Components like TextInput and View, as well as the stylesheet and images. got an error with image

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image } from 'react-native';
+
+
 
 export default function App() {
   console.log("App executed, but not dead"); 
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
+      <TextInput style={{
+          height: 40,
+          width: 100,
+          borderColor: 'gray',
+          borderWidth: 1}}
+          defaultValue="Hi, app"
+          />
+          {/* <Image source="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQAO4JaTYCaqGzut4Ie4kSFQB8pENgbBP47A&usqp=CAU"></Image> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +25,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+// const tina = {
+//   uri="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQAO4JaTYCaqGzut4Ie4kSFQB8pENgbBP47A&usqp=CAU",
+//   height=64,
+//   width=64
+// },
+
 });
